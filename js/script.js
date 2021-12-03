bf6f3eff6aa9a452de46f838a9cdf083f12edf6a
jacopo35 <jaczor00@gmail.com>
2021-12-03 15:18:20 +0100

    add-html&js-first commands

--- js/script.js
+++ js/script.js
@@ -4,3 +4,55 @@ Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomFunc() {
+  let randomNumber = [];
+ while (randomNumber.length < 5) {
+    let random = getRndInteger(1, 5)
+ while (randomNumber.includes(random)) {
+        random = getRndInteger(1, 5)
+        }
+        randomNumber.push(random);
+    }
+    return randomNumber
+}
+
+//Generare numeri random
+let randNum = randomFunc();
+
+console.log(randNum);
+const container = document.querySelector(".container");
+
+for (let i = 0; i < randNum.length; i++) {
+  container.innerHTML += randNum[i] + " ";
+}
+let arrayNum = []
+
+
+
+setTimeout(() => {
+  container.innerHTML = ""
+}, 3000)
+
+//Indicare i numeri visti
+setTimeout(() => {
+ for (let i = 0; i < 5; i++) {
+      let ask = parseInt(prompt(""))
+       arrayNum.push(ask);
+    }
+}, 4000)
+
+console.log(arrayNum);
+
+//Dichiarare la vittoria o la sconfitta dell'utente
+for (let i = 0; i < 5; i++) {
+ if (randNum[i] == arrayNum[i]) {
+        console.log("");
+ } else {
+    console.log("");
+    }
+
+}

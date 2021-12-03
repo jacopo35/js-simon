2b225f5c76c0e42f32f1e182a419c62e6d785b37
jacopo35 <jaczor00@gmail.com>
2021-12-03 14:53:54 +0100

    first-commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/*
+Visualizzare in pagina 5 numeri casuali poi fateli sparire.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
